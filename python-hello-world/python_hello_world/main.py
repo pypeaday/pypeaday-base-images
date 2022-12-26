8bf042d8aaabf60f9dd59f880d9269446d403b3d
Pypeaday <pypeaday@pm.me>
2022-12-25 20:42:43 -0600

    add APP_VERSION to hello world

--- python-hello-world/python_hello_world/main.py
+++ python-hello-world/python_hello_world/main.py
@@ -1,9 +1,12 @@
+import os
+
 import typer
 from rich import print
 
 
 def main():
     print("[bold red]Alert![/bold red] [green]Portal gun[/green] shooting! :boom:")
+    print(f"APP VERSION = {os.environ.get('APP_VERSION', 'NO VERSION FOUND')}")
     return 0
 
 

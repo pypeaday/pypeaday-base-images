20a90ca1fac950f7b930f57c76496b7c74102cba
Pypeaday <pypeaday@pm.me>
2022-12-26 12:59:01 -0600

    add a type hinted function with test examples

--- python-hello-world/python_hello_world/main.py
+++ python-hello-world/python_hello_world/main.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any, Dict, List, Union
 
 import typer
 from rich import print
@@ -9,6 +10,35 @@ def main():
     print("[bold red]Alert![/bold red] [green]Portal gun[/green] shooting! :boom:")
     print(f"APP VERSION = {os.environ.get('APP_VERSION', 'NO VERSION FOUND')}")
     return 0
+
+
+def a_type_hinted_function(
+    string_argument: str,
+    integer_argument: int,
+    integer_or_float_argument: Union[int, float],
+) -> Dict[str, List[Any]]:
+    """Summary of a_type_hinted_function.
+
+    Type hints in Python enable safer and faster development with the help of the LSP.
+    Note that types are not enforced by the interpreter at all...
+    ie. Type hinting doesn't affect the runtime of a script... they're hints, not law
+
+    Args:
+        string_argument: An argument with type designated as str
+        integer_argument: An argument with type designated as int
+        integer_or_float_argument: An argument that can be either an integer or a float
+    """
+    assert isinstance(string_argument, str)
+    assert isinstance(integer_argument, int)
+    assert isinstance(integer_or_float_argument, int) or isinstance(
+        integer_or_float_argument, float
+    )
+    return {
+        "example key": list(range(10)),
+        "string argument": string_argument,
+        "integer argument": integer_argument,
+        "integer or float argument": integer_or_float_argument,
+    }
 
 
 if __name__ == "__main__":
--- python-hello-world/tests/test_main.py
+++ python-hello-world/tests/test_main.py
@@ -1,4 +1,24 @@
+import pytest
+
 from python_hello_world import main
+
+
+@pytest.mark.parametrize(
+    ("x", "y", "z", "error"),
+    [
+        ("a string", 69, 42.0, None),
+        ("a string", 69, 420, None),
+        (1111, 69, 420, AssertionError),
+        ("a string", 69.0, 420, AssertionError),
+        ("a string", 69, "420", AssertionError),
+    ],
+)
+def test_a_type_hinted_function(x, y, z, error):
+    if error is None:
+        r = main.a_type_hinted_function(x, y, z)
+    else:
+        with pytest.raises(AssertionError):
+            main.a_type_hinted_function(x, y, z)
 
 
 def test_main():

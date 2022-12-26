30cfaf6c86aa7614c0340082d558a2c1e8c5b336
Pypeaday <pypeaday@pm.me>
2022-12-26 13:07:33 -0600

    add tests for result

--- python-hello-world/tests/test_main.py
+++ python-hello-world/tests/test_main.py
@@ -16,6 +16,9 @@ from python_hello_world import main
 def test_a_type_hinted_function(x, y, z, error):
     if error is None:
         r = main.a_type_hinted_function(x, y, z)
+        assert r["string argument"] == x
+        assert r["integer argument"] == y
+        assert r["integer or float argument"] == z
     else:
         with pytest.raises(AssertionError):
             main.a_type_hinted_function(x, y, z)
